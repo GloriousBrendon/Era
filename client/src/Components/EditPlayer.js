84260b7babc0b8faa100b3c764aa72e914fe2ecd
Brendon Venter <74231476+GloriousBrendon@users.noreply.github.com>
2022-11-08 12:20:38 +0200

    Edit function

--- client/src/Components/EditPlayer.js
+++ client/src/Components/EditPlayer.js
@@ -1,13 +1,23 @@
 import {useEffect, useState} from 'react';
 import {
-    Center, Input, Select, Spinner, FormLabel
+    Center, Input, Select, Spinner, FormLabel, Button, Text, Box,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton
   } from "@chakra-ui/react";
 
 function EditPlayer({editId, teams}) {
 
     const [player, setPlayer] = useState()
 
+    const [resultMessage, setResultMessage] = useState()
+
     const [editData, setEditData] = useState()
+
+    const positions = ['Top', 'Jungle', 'Mid', 'Bot', 'Supp', 'Sub']
 
     useEffect(() => {
         if (player !== undefined) {
@@ -23,6 +33,23 @@ function EditPlayer({editId, teams}) {
 
     function updateData(e) {
         setEditData({ ...editData, [e.target.id] : e.target.value });
+    }
+
+    function saveEditData() {
+        fetch(`http://127.0.0.1:3333/players/${editId}`, {
+            method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(editData)
+        }).then(result => {
+            if (result.ok){
+            setResultMessage("Saved")
+        }
+        else {
+            setResultMessage("Error" + result.message)
+        }})
+        .catch(() => setResultMessage("Error"))
     }
 
     useEffect(() => {
@@ -46,12 +73,22 @@ function EditPlayer({editId, teams}) {
     ) 
   
     return (
+        <>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{player.name}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
         <Center>
             <form>
                 <FormLabel>Name</FormLabel>
                 <Input id="name" onChange={updateData} value={player.name} />
                 <FormLabel>Position</FormLabel>
-                <Input id="position" onChange={updateData} value={player.position} />
+                <Select id="position" onChange={updateData} value={player.position}>
+                {positions.map(p => 
+                    <option value={p}>{p}</option>
+                    )}
+                </Select>
                 <FormLabel>Active</FormLabel>
                 <Select id="active" onChange={updateData} value={player.active}>
                     <option value={true}>Yes</option>
@@ -63,8 +100,17 @@ function EditPlayer({editId, teams}) {
                     <option value={t.id}>{t.team_name}</option>
                     )}
                 </Select>
+                <Button colorScheme="green" variant="outline" m={4} onClick={saveEditData}>Save</Button>
+                <Button colorScheme="red" variant="outline" m={4} onClick={updateData}>Cancel</Button>
+                <Box textAlign="center">
+                    <Text color="red">{resultMessage}</Text>
+                </Box>
+                
             </form>
         </Center>
+        </ModalBody>
+        </ModalContent>
+        </>
     );
   };
   export default EditPlayer;
--- client/src/Components/PlayersTable.js
+++ client/src/Components/PlayersTable.js
@@ -11,12 +11,6 @@ import {
   TableContainer,
   Button,
   Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton
   } from "@chakra-ui/react";
 import SimpleDateTime  from 'react-simple-timestamp-to-date';
 import { useDisclosure } from '@chakra-ui/react'
@@ -35,21 +29,7 @@ function PlayersTable({players, teams}) {
     return (
         <>
         <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
             <EditPlayer teams={teams} editId={editId} />
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant='ghost'>Secondary Action</Button>
-          </ModalFooter>
-        </ModalContent>
       </Modal>
 
         <TableContainer>
