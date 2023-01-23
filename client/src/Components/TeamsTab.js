8ae0453fcd02ff87bd190a9440205a6f602af53e
Brendon Venter <74231476+GloriousBrendon@users.noreply.github.com>
2023-01-23 16:28:11 +0200

    Display teams correctly

--- client/src/Components/TeamsTab.js
+++ client/src/Components/TeamsTab.js
@@ -1,6 +1,7 @@
 import { Box, Center, Divider, Flex, Grid, GridItem, Heading, Image, Stack, Text } from "@chakra-ui/react";
 import { useEffect, useState } from 'react';
 import getPlayers from '../APIUtils/getPlayers';
+import { Link } from "react-router-dom";
 
 function TeamTab({teams}) {
 
@@ -19,34 +20,40 @@ function TeamTab({teams}) {
   
     else return (
         <>
-            {teams.map(x => x.active === 1 ? 
-            <Box m={2} p={3} bgGradient={`linear(to-r, dark, ${x.team_name}, dark)`} borderBottom={`solid 1px ${x.team_name}`} key={x.id} justifyContent="center" minHeight="20vh">
-              <Stack p={2} bg='dark'>
+        <div class="grid md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-11">
+            {teams.map(x => x.active === 1 ?
+            <>
+              <Link to={`${x.team_name}`} className="transition duration-300 md:p-4 py-2 block text-[#E8E8E8] hover:text-[#922729]"
+                >
+              <Box m={2} p={3} bgGradient={`linear(to-r, dark, ${x.team_name}, dark)`} borderBottom={`solid 1px ${x.team_name}`} key={x.id} justifyContent="center" minHeight="20vh">
+              <Stack text-align-center p={2} bg='dark'>
               <Heading>{x.team_name}</Heading>
-              <Grid templateColumns='repeat(5, 1fr)' gap={3}>
-                {
+                  {
                   positions.map(p => 
                     <GridItem w='100%'>
-                      <Image h="40%" w="auto" src={require(`../Img/${p}.png`)} />
+                      <Flex>
+                      <Image h="50px" w="auto" src={require(`../Img/${p}.png`)} />
                       {players.map(y =>
                       <>
                           {(y.team_id === x.id && y.position === p.toString()) ? 
-                          <Stack>
-                          <a href={`https://www.op.gg/summoners/euw/${y.name}`}><Heading size="md">{y.name}</Heading></a>
-                          </Stack>
+                          <h1 className="text-3xl font-semibold pb-5">{y.name}</h1>
                           :
                           <></>
                       }
                       </>
                       )}
+                      </Flex>
                     </GridItem>
                   )
                 }
-              </Grid>
+                
               </Stack>
             </Box>
+              </Link>
+            </>
             :
             <></>)}
+            </div>
         </>
     );
   };
