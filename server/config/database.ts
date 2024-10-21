6a250a54e6c918cf7c9f540ae303de58b505285b
Brendon Venter <74231476+GloriousBrendon@users.noreply.github.com>
2024-10-21 14:18:28 +0200

    Changes and shit

--- server/config/database.ts
+++ server/config/database.ts
@@ -34,18 +34,18 @@ const databaseConfig: DatabaseConfig = {
     |
     */
     mysql: {
-      client: 'mysql2',
+      client: 'mysql',
       connection: {
         host: Env.get('MYSQL_HOST'),
         port: Env.get('MYSQL_PORT'),
         user: Env.get('MYSQL_USER'),
-        password: Env.get('MYSQL_PASSWORD', ''),
+        password: Env.get('MYSQL_PASSWORD'),
         database: Env.get('MYSQL_DB_NAME'),
       },
       migrations: {
         naturalSort: true,
       },
-      healthCheck: false,
+      healthCheck: true,
       debug: false,
     },
 
