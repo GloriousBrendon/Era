d1f254a3d15294bfeae311eb0e7752764073d276
Brendon Venter <74231476+GloriousBrendon@users.noreply.github.com>
2024-10-22 15:21:51 +0200

    changed stuff

--- client/src/Components/Hero.js
+++ client/src/Components/Hero.js
@@ -1,30 +1,41 @@
-import { Center, Stack, Image, Heading, Box, Flex, Grid,GridItem } from "@chakra-ui/react";
-import introversion from "../Img/IntroversionFull.png"
-import hero2 from "../Img/hero2.jpg"
+import {
+	Center,
+	Stack,
+	Image,
+	Heading,
+	Box,
+	Flex,
+	Grid,
+	GridItem,
+} from "@chakra-ui/react";
+import introversion from "../Img/IntroversionFull.png";
+import hero2 from "../Img/hero2.jpg";
+import Upcoming from "./Upcoming";
 
 function Hero() {
-  
-    return (
-            <Center
-            minH="100vh">
-            <Grid templateColumns='repeat(2, 1fr)'>
-            <Center>
-            <GridItem p="4" textAlign="end" colSpan={1} >
-            <Heading color="white" size="3xl">EVERY </Heading>
-            <Heading color="#B026FF" size="4xl">DREAM</Heading>
-            <Heading size="2xl">begins with a</Heading>
-            <Heading color="#B026FF" size="3xl">DREAMER</Heading>
-            <Heading>#StartDreaming</Heading>
-            </GridItem>
-            </Center>
-            <GridItem bgImage={hero2}
-            bgPosition="center"
-            bgRepeat="no-repeat"
-            bgSize="cover" colSpan={1} >
-            <Image src={introversion} h="500" w="auto" />
-            </GridItem>
-            </Grid>
-            </Center>
-    );
-  };
-  export default Hero;
+	return (
+		<Center minH='100vh'>
+			<Grid templateColumns='repeat(2, 1fr)'>
+				<Center>
+					<GridItem p='4' textAlign='end' colSpan={1}>
+						<Heading color='white' size='3xl'>
+							WELCOME{" "}
+						</Heading>
+						<Heading color='#B026FF' size='4xl'>
+							TO
+						</Heading>
+						<Heading size='2xl'>MY ESPORTS</Heading>
+						<Heading color='#B026FF' size='3xl'>
+							PLACE
+						</Heading>
+						<Heading>#WIN</Heading>
+					</GridItem>
+				</Center>
+				<GridItem>
+					<Upcoming />
+				</GridItem>
+			</Grid>
+		</Center>
+	);
+}
+export default Hero;
---
+++ client/src/Components/Upcoming.js
@@ -0,0 +1,10 @@
+import { Center, Heading } from "@chakra-ui/react";
+
+function Upcoming() {
+	return (
+		<Center borderRadius={10} bg='#1F2022' minH='80vh'>
+			<Heading>Upcoming matches</Heading>
+		</Center>
+	);
+}
+export default Upcoming;
